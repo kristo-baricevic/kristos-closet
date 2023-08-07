926f49faacae4f0cf8e344f12726e80ba83755ef
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-07 16:55:51 -0400

    initial commit

--- client/closet-app/src/App.js
+++ client/closet-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is <code>src/App.js</code> a React app
         </p>
         <a
           className="App-link"
