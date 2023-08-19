5262a3887afc89ebf68f8d18c121b393d36c9ec5
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-19 10:40:10 -0400

    changed selectedItemsSlice

--- client/closet-app/src/features/selectedItemsSlice.js
+++ client/closet-app/src/features/selectedItemsSlice.js
@@ -3,8 +3,12 @@ import { createSlice } from '@reduxjs/toolkit';
 const selectedItemsSlice = createSlice({
   name: 'selectedItems',
   initialState: {
-    closet: [],
-    accessory: [],
+    top: null,
+    bottom: null,
+    shoes: null,
+    hat: null,
+    onePiece: null,
+    accessories: [],
   },
   reducers: {
     addItem: (state, action) => {
@@ -18,22 +22,26 @@ const selectedItemsSlice = createSlice({
 
       // If item is a 'onePiece', remove 'top' and 'bottom' from the state
       if (item.category === 'onePiece') {
-        state.closet = state.closet.filter(selectedItem => selectedItem.category !== 'top' && selectedItem.category !== 'bottom');
+        state.top = null;
+        state.bottom = null;
       } else if (item.category === 'top' || item.category === 'bottom') {
         // If item is 'top' or 'bottom', remove 'onePiece' from the state
-        state.closet = state.closet.filter(selectedItem => selectedItem.category !== 'onePiece');
+        state.onePiece = null;
       }
 
       // Ensure that only up to three accessories can be added
-      if (category === 'accessory' && state.accessory.length >= 3) {
-        console.error('Too many accessories:', state.accessory);
+      if (category === 'accessories' && state.accessories.length >= 3) {
+        console.error('Too many accessories:', state.accessories);
         return;
       }
 
-      state[category].push(item);
+      // Update the state with the new item for the specified category
+      state[category] = Array.isArray(state[category]) ? [...state[category], item] : item;
     },
     removeItem: (state, action) => {
       const { category, itemId } = action.payload;
+      
+      // Update the state by removing the item from the specified category
       state[category] = state[category].filter(item => item.id !== itemId);
     },
   },
@@ -41,16 +49,16 @@ const selectedItemsSlice = createSlice({
 
 // Function to check if adding a certain item is valid based on constraints
 function isValidAddition(state, category, newItem) {
-  if (category === 'accessory' && state.accessory.length >= 3) {
+  if (category === 'accessories' && state.accessories.length >= 3) {
     return false; // Too many accessories
   }
 
   if (newItem.category === 'onePiece') {
-    return !state.closet.some(item => item.category === 'top' || item.category === 'bottom');
+    return !state.top && !state.bottom;
   }
 
   if (newItem.category === 'top' || newItem.category === 'bottom') {
-    return !state.closet.some(item => item.category === 'onePiece');
+    return !state.onePiece;
   }
 
   return true;
