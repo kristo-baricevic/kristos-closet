fb8b2234e540e3fd1bd954c7da28503cfd799d91
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-21 18:51:37 -0400

    initial commit

--- client/closet-app/src/features/userSlice.js
+++ client/closet-app/src/features/userSlice.js
@@ -1,20 +1,116 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    user: null,
+    isAuthenticated: false,
+};
+
+
+// Async thunk action to register user
+export const registerUser = createAsyncThunk(
+    "user/registerUser",
+    async (userData) => {
+        try {
+        console.log("userData about to post");
+        const response = await axios.post(
+            "http://localhost:5000/api/register",
+            userData
+        );
+        console.log("userData posted");
+
+        if (response.status === 201) {
+            return response.data;
+        }
+        } catch (error) {
+        throw error;
+        }
+    }
+);
+
+// Async thunk action to log in user
+export const loginUser = createAsyncThunk(
+    "user/loginUser",
+    async (userData) => {
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/api/login",
+          userData
+        );
+  
+        if (response.status === 200) {
+          const user = response.data.user;
+          const token = response.data.token;
+  
+          // Set the token in client storage
+          localStorage.setItem("token", token);
+  
+          return user;
+        }
+      } catch (error) {
+        throw error;
+      }
+    }
+);
+
+// Create an async thunk for loginAnonymous
+export const loginAnonymous = createAsyncThunk(
+    "user/loginAnonymous",
+    async () => {
+      try {
+        const response = await axios.post('http://localhost:5000/api/loginAnonymous');
+        const isAuthenticated = response.data.isAuthenticated;
+        const user = response.data.user;
+        const token = response.data.token;
+    
+        localStorage.setItem('token', token);
+    
+        return { isAuthenticated, user };
+      } catch (error) {
+        throw error;
+      }
+    }
+  );
 
 export const userSlice = createSlice({
     name: "user",
-    initialState: {
-        user: null,
-        isAuthenticated: false,
-    },
+    initialState,
     reducers: {
         login: (state, action) => {
-            state.user = action.payload.user;
+            state.user = action.payload;
             state.isAuthenticated = true;
         },
         logout: (state) => {
             state.user = null;
             state.isAuthenticated = false;
         },
+        setAuthentication: (state, action) => {
+            state.isAuthenticated = action.payload;
+          },
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+        clearAuthentication: (state) => {
+            state.isAuthenticated = false;
+            state.user = null;        
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.isAuthenticated = true;
+                state.user = action.payload;
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                // Handle login failure if needed
+            })
+            .addCase(loginAnonymous.fulfilled, (state, action) => {
+                state.isAuthenticated = action.payload.isAuthenticated;
+                state.user = action.payload.user;
+            })
+            .addCase(loginAnonymous.rejected, (state, action) => {
+                // Handle the error case here if needed
+            });
     },
 });
 
--- client/closet-app/src/store/store.js
+++ client/closet-app/src/store/store.js
@@ -1,19 +1,11 @@
 import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
+import reducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
 import selectedItemsReducer from '../features/selectedItemsSlice'; 
 
-const middleware = [...getDefaultMiddleware(), thunkMiddleware];
-
-const rootReducerCombined = combineReducers({
-  selectedItems: selectedItemsReducer,
-  reducer: rootReducer,
+const store = configureStore({
+  reducer: 
 })
 
-const store = configureStore({
-  reducer: rootReducerCombined,
-  middleware: middleware,
-  devTools: process.env.NODE_ENV !== 'production',
-});
 
 export default store;
