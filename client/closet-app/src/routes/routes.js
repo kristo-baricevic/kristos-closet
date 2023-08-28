f02ac226cbf8b7c803e4f60f9849b91654c2e907
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-28 17:33:52 -0400

    changed ...

--- client/closet-app/src/routes/routes.js
+++ client/closet-app/src/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
 
 const { verifyToken } = require('../middleware/authMiddleware');
 const userController = require('../controllers/userController');
