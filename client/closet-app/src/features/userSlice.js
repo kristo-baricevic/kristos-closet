d7567c8ab22c9355b882b99093f379ee1720c632
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-28 13:35:59 -0400

    fixed url

--- client/closet-app/src/features/userSlice.js
+++ client/closet-app/src/features/userSlice.js
@@ -60,7 +60,7 @@ export const loginAnonymous = createAsyncThunk(
   async () => {
     try {
       console.log("inside loginAnonymous");
-      const response = await axios.post('kristos-closet.vercel.app/api/loginAnonymous');
+      const response = await axios.post('https://kristos-closet.vercel.app/api/loginAnonymous');
       const isAuthenticated = response.data.isAuthenticated;
       console.log("response", isAuthenticated);
       const user = response.data.user;
