1d957b3a852ab5d13fff4ed03669d7e1bfce0f55
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-28 13:38:55 -0400

    removed websocket

--- client/closet-app/src/index.js
+++ client/closet-app/src/index.js
@@ -9,19 +9,6 @@ import 'path-browserify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-// Create a WebSocket connection
-const socket = new WebSocket('wss://closet-app-backend.fly.dev/ws');
-
-// Attach event listeners to the socket (e.g., for handling messages)
-socket.addEventListener('open', () => {
-  console.log('WebSocket connection opened');
-});
-
-socket.addEventListener('message', (event) => {
-  console.log('Received message:', event.data);
-});
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
