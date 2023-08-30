2370d3b4e47cd7f8d82cad7709a8ff1d6c550c59
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-30 18:23:32 -0400

    noCoors

--- client/closet-app/backend/start.js
+++ client/closet-app/backend/start.js
@@ -2,20 +2,20 @@
 const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
-const corsOptions = {
-  origin: [
-    'http://localhost:3000', 
-    'http://kristos-closet.vercel.app' 
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+// const corsOptions = {
+//   origin: [
+//     'http://localhost:3000', 
+//     'http://kristos-closet.vercel.app' 
+//   ],
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// };
 
 require('dotenv').config();
-app.use(cors(corsOptions)); 
+// app.use(cors(corsOptions)); 
 
 
 app.use(express.json()); 
