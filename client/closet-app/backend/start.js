96e500bb0059b305767ea15d7abd5e42b2e11ab0
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-30 17:59:51 -0400

    pairing down

--- client/closet-app/backend/start.js
+++ client/closet-app/backend/start.js
@@ -1,9 +1,22 @@
 //start server in production
-const app = require('./server.js');
+// const app = require('./server.js');
+const express = require('express');
 const port = process.env.PORT || 5000;
+const app = express();
 
 require('dotenv').config();
 
+app.use(express.json()); 
+app.use('/', (req, res) => {  
+  console.log('Request to walk-in received.');
+  res.send('Welcome to the Walk-In Closet');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Internal closet error' });
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is definitely running on port ${port}`);
 });
