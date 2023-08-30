f4db7bebd55b935cb0703f8a3c58f2e9168e6bf7
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-30 17:19:01 -0400

    idk

--- client/closet-app/backend/server.js
+++ client/closet-app/backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 // const corsOptions = {
 //   origin: [
 //     'http://localhost:3000', 
-//     'http://kristos-closet.vercel.app'
+//     'http://kristos-closet.vercel.app' 
 //   ],
 //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 //   credentials: true,
