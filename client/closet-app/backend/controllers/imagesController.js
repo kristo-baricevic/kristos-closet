517314f309d2f41a2fa18821728b34db574476e1
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-30 15:31:54 -0400

    created backend dir

---
+++ client/closet-app/backend/controllers/imagesController.js
@@ -0,0 +1,120 @@
+const ClothingItem = require('../models/ClothingItem');
+const AWS = require('aws-sdk');
+
+exports.getImages = async (req, res) => {
+  try {
+    const { user } = req.body;
+
+    console.log("inside get images");
+
+    const clothingItems = await ClothingItem.find({ $or: [{ user }, { isUserImage: false }] });
+
+    console.log("clothing items", clothingItems);
+
+    const images = await Promise.all(clothingItems.map(async item => {
+      const s3 = new AWS.S3();
+      const bucketName = 'closet-app';
+      const imageKey = `${item.imageUrl}`; 
+
+      console.log("item filename check", item.imageUrl);
+      console.log("imageKey check", imageKey);
+
+      const imageObject = await s3.getObject({
+        Bucket: bucketName,
+        Key: imageKey,
+      }).promise();
+
+      console.log(imageObject);
+
+      const imageUrl = `https://kristos-closet.vercel.app/api/images/${item._id}`;
+
+      console.log("backend URL", imageUrl);
+
+      return {
+        id: item._id,
+        imageUrl: imageUrl, 
+        category: item.category,
+        userId: item.userId,
+        filename: item.filename,
+        contentType: item.contentType
+      };
+    }));
+
+    res.json(images);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
+
+exports.getImageById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const item = await ClothingItem.findById(id);
+
+    if (!item) {
+      return res.status(404).json({ error: 'Image not found' });
+    }
+
+    const s3 = new AWS.S3();
+    const bucketName = 'closet-app';
+    const imageKey = `${item.imageUrl}`; 
+
+    const imageObject = await s3.getObject({
+      Bucket: bucketName,
+      Key: imageKey,
+    }).promise();
+
+    // Send the image as a response
+    res.set('Content-Type', item.contentType);
+    res.send(imageObject.Body);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
+
+exports.updateImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { category } = req.body;
+
+    const clothingItem = await ClothingItem.findOneAndUpdate(
+      { _id: id },
+      { $set: { category } },
+      { new: true }
+    );
+
+    if (!clothingItem) {
+      return res.status(404).json({ error: 'Image not found' });
+    }
+
+    res.json({
+      id: clothingItem._id,
+      data: clothingItem.image,
+      category: clothingItem.category,
+      userId: clothingItem.userId,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
+
+
+exports.deleteImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const clothingItem = await ClothingItem.findOneAndDelete({ _id: id });
+
+    if (!clothingItem) {
+      return res.status(404).json({ error: 'Image not found' });
+    }
+
+    res.json({ message: 'Image deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
--- client/closet-app/server.js
+++ client/closet-app/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const routes = require('./src/routes/routes');
+const routes = require('./backend/routes/routes');
 // const mongoose = require('mongoose');
 // const cors = require('cors');
 // const dotenv = require('dotenv');
