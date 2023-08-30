79eefb638947697d996b3382b44d8e5c0afdf26e
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-30 13:05:45 -0400

    added config

--- client/closet-app/server.js
+++ client/closet-app/server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 // dotenv.config({ path: '/Users/kristo/kristos-closet/client/closet-app/.env.local' });
-
+dotenv.config();
 
 // Connect to the database
 console.log("DB_URI:", process.env.MONGODB_URI);
