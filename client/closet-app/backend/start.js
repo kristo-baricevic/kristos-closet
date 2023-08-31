e3466d1cfd5d4070a9d63acad442b214179a509f
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-31 17:11:47 -0400

    cors syntax

--- client/closet-app/backend/start.js
+++ client/closet-app/backend/start.js
@@ -26,7 +26,7 @@ const ApiRouter = require('./routes/routes');
 require('dotenv').config();
 
 // app.use(cors(corsOptions)); 
-app.use(cors);
+app.use(cors());
 
 app.use(express.json()); 
 app.use('/', (req, res, next) => {  
