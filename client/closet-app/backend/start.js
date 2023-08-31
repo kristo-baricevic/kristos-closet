2fdabff083247a1c92061c85a9a4e397da42e1ee
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-08-31 10:12:48 -0400

    routes

--- client/closet-app/backend/start.js
+++ client/closet-app/backend/start.js
@@ -3,7 +3,7 @@ const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
 const cors = require('cors');
-const routes = require('./routes/routes');
+// const routes = require('./routes/routes');
 
 
 const corsOptions = {
