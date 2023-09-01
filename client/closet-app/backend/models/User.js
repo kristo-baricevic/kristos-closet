b8f98b950efa219f302d89e3107b1abf7cfd29c7
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-09-01 11:13:46 -0400

    schema

--- client/closet-app/backend/models/User.js
+++ client/closet-app/backend/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: String,
@@ -9,28 +9,28 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash the user's password before saving it to the databasee
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) {
+//     return next();
+//   }
 
-  try {
-    const hashedPassword = await bcrypt.hash(this.password, 10);
-    this.password = hashedPassword;
-    return next();
-  } catch (error) {
-    return next(error);
-  }
-});
+//   try {
+//     const hashedPassword = await bcrypt.hash(this.password, 10);
+//     this.password = hashedPassword;
+//     return next();
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
 
 // Compare the provided password with the stored hashed password
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  try {
-    return await bcrypt.compare(candidatePassword, this.password);
-  } catch (error) {
-    throw error;
-  }
-};
+// userSchema.methods.comparePassword = async function (candidatePassword) {
+//   try {
+//     return await bcrypt.compare(candidatePassword, this.password);
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
 const User = mongoose.model('User', userSchema);
 
