f61ae1b71104cbf72fc0df07ad01e8d26bc29e22
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-09-01 14:12:50 -0400

    added back in routes p1

--- client/closet-app/backend/routes/routes.js
+++ client/closet-app/backend/routes/routes.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const app = express();
 
+const { verifyToken } = require('../middleware/authMiddleware');
+const userController = require('../controllers/userController');
+const uploadController = require('../controllers/uploadController');
+const upload = require('../config/multer-config');
+const {
+  getImages,
+  getImageById,
+  updateImage,
+  deleteImage,
+} = require('../controllers/imagesController');
+
+
+// Image routes
+router.get('/images', getImages);
+router.put('/images/:id', verifyToken, updateImage);
+router.get('/images/:id', getImageById);
+router.delete('/images/:id', verifyToken, deleteImage);
 
 router.post('/loginAnonymous', userController.loginAnonymous);
 // router.get('/current', verifyToken, userController.getCurrentUserData);
