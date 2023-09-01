c5773a89b1d62020b2555b10262d182f0582d3f6
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-09-01 14:34:16 -0400

    added back in cors

--- client/closet-app/backend/start.js
+++ client/closet-app/backend/start.js
@@ -1,40 +1,37 @@
-//start server in production
+// start server in production
 const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
 const cors = require('cors');
-// const {
-//   getImages,
-//   getImageById,
-//   updateImage,
-//   deleteImage,
-// } = require('./controllers/imagesController');
-// const { verifyToken } = require('./middleware/authMiddleware');
 const ApiRouter = require('./routes/routes');
 const dotenv = require ('dotenv');
 const mongoose = require('mongoose');
 
 
-// const corsOptions = {
-//   origin: [
-//     'http://localhost:3000', 
-//     'http://kristos-closet.vercel.app'
-//   ],
-//   methods: 'GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE',
-//   credentials: true,
-//   optionsSuccessStatus: 204,
-// };
+// set-up cors
+const corsOptions = {
+  origin: [
+    'http://localhost:3000', 
+    'http://kristos-closet.vercel.app'
+  ],
+  methods: 'GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-
+// local variables for local testing
 // dotenv.config({ path: './.env' });
 
+// local variables for deloployment environment
 dotenv.config();
 console.log("env loaded 1/2 ", process.env.MONGODB_URI);
+
+// test connection to mongo db
 mongoose.connect(process.env.MONGODB_URI);
 console.log("passed the mongoose!");
 
-// app.use(cors(corsOptions)); 
-app.use(cors());
+app.use(cors(corsOptions)); 
+// app.use(cors());
 
 app.use(express.json()); 
 
