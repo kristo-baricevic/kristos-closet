e2cf89754c15b0b522336430335e9be0e943d310
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-11-02 13:45:39 -0400

    updated css

---
+++ client/closet-app/src/routes/routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const { verifyToken } = require('../middleware/authMiddleware');
+const { singleUpload } = require('../middleware/uploadMiddleware');
+const userController = require('../controllers/userController');
+const {
+  getImages,
+  updateImage,
+  deleteImage,
+} = require('../controllers/imagesController');
+const {
+  uploadImage,
+  handleUpload,
+} = require('../controllers/uploadController');
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  loginAnonymous,
+  getCurrentUserData,
+} = require('../controllers/userController');
+
+// Image routes
+router.get('/images', verifyToken, getImages);
+router.put('/images/:id', verifyToken, updateImage);
+router.delete('/images/:id', verifyToken, deleteImage);
+
+// Upload route
+router.post('/upload', singleUpload, handleUpload);
+
+// User routes
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.post('/logout', verifyToken, logoutUser);
+router.post('/loginAnonymous', loginAnonymous);
+router.get('/current', verifyToken, getCurrentUserData);
+
+module.exports = router;
