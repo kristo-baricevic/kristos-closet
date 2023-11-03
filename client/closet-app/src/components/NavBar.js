c8fb3a9ce69fbf3bb9afedb459b508cca9730862
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-11-03 10:52:09 -0400

    small css

--- client/closet-app/src/components/NavBar.js
+++ client/closet-app/src/components/NavBar.js
@@ -31,7 +31,7 @@ const NavBar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-menu">
-                <div class="navbar-buttons-container">
+                <div className="navbar-buttons-container">
                     <button className="navbar-button" onClick={showRegistrationModal}>Register</button>
                     {!isAuthenticated ? (
                     <button className="navbar-button" onClick={showLoginModal}>Login</button>
@@ -41,9 +41,9 @@ const NavBar = () => {
                     <DemoButton />
                 </div>
                     <ImageUploader />
-                <div>
+                <div className="logo-container">
                     <Link to="/" className="logo">
-                        <img class="logo-sizing" src="https://raw.githubusercontent.com/kristo-baricevic/kristos-closet/33c8e3961e096dbe555cf03f576f8c4ea28848cd/client/closet-app/src/wardrobe-modified.png" alt="nav logo" />
+                        <img className="logo-sizing" src="https://raw.githubusercontent.com/kristo-baricevic/kristos-closet/33c8e3961e096dbe555cf03f576f8c4ea28848cd/client/closet-app/src/wardrobe-modified.png" alt="nav logo" />
                     </Link>
                 </div>
             </div>
