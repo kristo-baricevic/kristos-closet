c5f150879ca427ede181e0723fc94f9ad88679dd
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-11-11 19:18:01 -0500

    type

--- client/closet-app/src/models/SavedOutfit.js
+++ client/closet-app/src/models/SavedOutfit.js
@@ -13,7 +13,7 @@ const savedOutfitSchema = new Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   },
-  clothingItems: [{
+  outfit: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'ClothingItem',
   }],
